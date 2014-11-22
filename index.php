fe1c5f5adad41839985192f0d037ad3cff3a1753
dcsena <dcsena@umich.edu>
2014-11-22 17:17:48 -0600

    css2

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>StudyBuddy</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
 </head>
 <body>
 	<?php
