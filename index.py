47aeed7d59414fce1f7b709e2f53f984a7cd7c11
dcsena <dcsena@umich.edu>
2014-11-22 14:57:28 -0600

    testttttt

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
+
+
+print("Hello World!!!")
