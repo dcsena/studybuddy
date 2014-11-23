54e9339bd57d4d4288e81d9592e517e688f4d373
Matt L <leiboldm@umich.edu>
2014-11-22 23:23:08 -0500

    bug fix

--- database.php
+++ database.php
@@ -3,8 +3,7 @@
 		var $conn;
 		var $dbh;
 		function Database() {
-			$this->conn = pg_connect("host=ec2-54-243-245-159.compute-1.amazonaws.com port=5432 dbname=d9ekpsg66labji user=awmugmpdemzddy password=ps-jTmFdmIIU8VMWs2A8nX_3eQ connect_timeout=5")
-				or echo "Could not connect";
+			$this->conn = pg_connect("host=ec2-54-243-245-159.compute-1.amazonaws.com port=5432 dbname=d9ekpsg66labji user=awmugmpdemzddy password=ps-jTmFdmIIU8VMWs2A8nX_3eQ connect_timeout=5");
 		}
 		function query($q) {
 			$this->dbh = pg_query($this->conn, $q) or die('Could not execute query: ' . pg_last_error());
