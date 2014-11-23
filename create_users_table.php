7b728103dc2f33fbb24194247c39a1ad104404c2
dcsena <dcsena@umich.edu>
2014-11-22 20:40:00 -0600

    lollllll

--- create_users_table.php
+++ create_users_table.php
@@ -1,6 +1,6 @@
 <?php
 	error_reporting(E_ALL);
-	$dbconn = pg_connect("host=ec2-54-243-245-159.compute-1.amazonaws.com port=5432 dbname=d9ekpsg66labji user=awmugmpdenzddy password=ps-jTmFdmIIU8VMWs2A8nX_3eQ connect_timeout=5")
+	$dbconn = pg_connect("host=ec2-54-243-245-159.compute-1.amazonaws.com port=5432 dbname=d9ekpsg66labji user=awmugmpdemzddy password=ps-jTmFdmIIU8VMWs2A8nX_3eQ connect_timeout=5")
 		or die('Could not connect: ' . pg_last_error());
 
 	$query = 'CREATE TABLE Users (
