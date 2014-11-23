483dea1f8ba8b6045e1f8957d10b3016bbac493d
dcsena <dcsena@umich.edu>
2014-11-22 22:38:16 -0600

    updated footer

--- footer.php
+++ footer.php
@@ -1,5 +1,15 @@
 <?php
-	$header = "<footer>Made by Matt Leibold and Dylan Sena at Wildhacks</footer>";
+	$header = "<footer>Made by Matt Leibold and Dylan Sena at Wildhacks";
 	echo $header;
+	$fileName = 'footer.php';
+			$fileName = $_SERVER["REQUEST_URI"];
+			$url_pieces = explode("/",$fileName);
+			$len = count($url_pieces);
+			$fileName = $url_pieces[$len-1];
+			if ($fileName == "")
+				$fileName = "index.php";
+			$timestamp = date('l jS \of F Y h:i:s A',filemtime($fileName));
+			echo $timestamp;
+	echo "</footer>";
 
 ?>
