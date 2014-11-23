b218b1e69f11a1bc7d7d40cbe74acedf1a2c9963
dcsena <dcsena@umich.edu>
2014-11-22 22:03:49 -0600

    login stuff

---
+++ process_login.php
@@ -0,0 +1,23 @@
+<?php
+	include("database.php");
+	$email = mysql_real_escape_string($_POST['email']);
+	$password = $_POST['password'];
+	$db = new Database();
+	$query = "SELECT * FROM Users WHERE email='" . $email . "'";
+	$db->query($query);
+	$result = $db->get_row();
+	if (!$result) {
+		echo "your email address is not in our system";
+	} else {
+		echo json_encode($result);
+		$hash = crypt($result['passwordsalt'] . $password);
+		if ($hash == $result['passwordhash']) {
+			echo "congrats, you've logged in";
+		} else {
+			echo "incorrrect password.  this incident has been reported";
+		}
+	}
+
+
+
+?>
