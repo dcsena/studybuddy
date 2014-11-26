1293fb944e50ee5b416fd46aac4d7bb9e1fa23d7
Matt L <leiboldm@umich.edu>
2014-11-25 22:41:20 -0500

    now displaying pairing on study.php

--- study.php
+++ study.php
@@ -4,14 +4,36 @@
 ?>
 	<h1> Meet friends in your classes. </h1>
 	<br/>
+<?php } ?>
 
-	<p>
-		Below are your upcoming appointments:
-	</p>
+<?php
+	require_once("database.php");
+	$db = new Database();
+	$email = $_SESSION['email'];
+	$db->query("SELECT * FROM Pairings WHERE email1='$email' OR email2='$email'");
+	$row = $db->get_row();
+	echo "<div class='main_image'>";
+	if ($row) {
+		echo "<p>Below are your current pairings:</p>";
+		$partnerEmail = $row['email1'];
+		if ($row['email1'] == $email) $partnerEmail = $row['email2'];
+		echo "<table class='pairing_table'><tr><th>Class</th><th>Partner Email</th></tr>";
+		echo "<tr><td>" . $row['class'] . "</td><td>" . $partnerEmail . "</td></tr>";
+		while ($row = $db->get_row()) {
+			$partnerEmail = $row['email1'];
+			if ($row['email1'] == $email) $partnerEmail = $row['email2'];
+			echo "<tr><td>" . $row['class'] . "</td><td>" . $partnerEmail . "</td></tr>";
+		}
+		echo "</table>";
+	} else {
+		echo "<p>You have no matches yet</p>";
+	}
+	echo "</div>";
+?>
 	
 	<br/>
 <?php
-	} else {
+	if (!isset($_SESSION['user'])) {
 		echo "<h1>Login or create an account to check your study times</h1>";
 	}
 	include("footer.php");
